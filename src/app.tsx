c4ac3ab5f69190d0ced7180bec41c9a79d731045
chinmaypant21 <pant.chinu.2@gmail.com>
2024-02-11 00:52:05 +0530

    Cursor support

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
+
 import './assets/styles/app.css'
 
 export function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>App</div>
+    <div>
+      <span>App</span>
+      <span className={'cursor-pointer'}>App</span>
+    </div>
   )
 }
