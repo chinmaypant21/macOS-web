53b9b6c83ee890e953b04551b32696f899042de9
chinmaypant21 <pant.chinu.2@gmail.com>
2024-02-11 02:20:41 +0530

    Absolute Imports | Fonts | Preloading

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { render } from 'preact'
+
 import { App } from './app.tsx'
-import './index.css'
+
+import '@assets/styles/index.css'
+import '@assets/styles/fonts.css'
 
 render(<App />, document.getElementById('app')!)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), viteTsconfigPaths()],
+  resolve: {
+    alias: {
+      'src': '/src',
+      '@assets': '/src/assets',
+      '@images': '/src/assets/images',
+      '@styles': '/src/assets/styles',
+      '@components': '/src/assets/components'
+    }
+  }
 })
