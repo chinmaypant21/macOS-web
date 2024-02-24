6d247713d95e5a88b6c1027112956f5f629abc65
chinmaypant21 <chinmay@growagro.in>
2024-02-24 16:31:47 +0530

    Dock context menu

--- src/layout/Dock/Dock.tsx
+++ src/layout/Dock/Dock.tsx
@@ -1,88 +1,19 @@
-
 import { activeWindows, presentFocusedWindow } from '@layout/Screen/Screen'
+
 import style from './Dock.module.css'
+import { Fragment } from 'preact/jsx-runtime'
+import DockApp from './components/DockApp'
 
 const Dock = () => {
-  function handleClick(selected_window: any){
-    if(selected_window.isMinimized) {
-      activeWindows.value = activeWindows.value.map((window) => {
-          if(window.index !== selected_window.index) {
-            return window
-          } 
-          else {
-            return {
-              ...window,
-              isMinimized: false
-            }
-          }
-      })
-
-      presentFocusedWindow.value = {
-        isActive: true,
-        windowId: selected_window.index
-      }
-    }
-
-    else if(
-      (selected_window.index === presentFocusedWindow.value.windowId) 
-      // && presentFocusedWindow.value.isActive
-    ){
-      activeWindows.value = activeWindows.value.map((window) => {
-        if(window.index !== selected_window.index) {
-          return window
-        } 
-        else {
-          return {
-            ...window,
-            isMinimized: true
-          }
-        }
-      })
-
-      presentFocusedWindow.value = {
-        windowId: 0,
-        isActive: false
-      }
-    }
-
-    else{
-      presentFocusedWindow.value = {
-        isActive: true,
-        windowId: selected_window.index
-      }
-    }
-  }
-
-  function toggleAnimation(event: any) {
-    const target = event.currentTarget;
-    target.classList.add(style['clicked']);
-  
-    setTimeout(() => {
-      target.classList.remove(style['clicked']);
-    }, 1000);
-  }
 
   return (
     <section id={style['dock-body']}>
       <div class={style['dock-container']}>
       {
         activeWindows.value.map((window) => (
-          <div
-            key={window.index}
-            onClick={(e)=>{
-              toggleAnimation(e)
-              handleClick(window)
-            }}
-            class={style['dock-item']}
-            tabIndex={0}
-          >
-            <span class={style['app-icon']}>{window.title}</span>
-            <div 
-              style={!window.isMinimized ? {backgroundColor: 'var(--color-green)' } : {}}
-              class={style['active-indicator']}
-            >
-            </div>
-          </div>
+          <Fragment key={window.index}>
+            <DockApp window={window} />
+          </Fragment>
         ))
       }
       </div>
---
+++ src/layout/Dock/components/DockApp.tsx
@@ -0,0 +1,136 @@
+import { useState } from 'preact/hooks'
+import { JSX } from 'preact/jsx-runtime'
+import { FC } from 'preact/compat'
+
+import { activeWindows, presentFocusedWindow } from '@layout/Screen/Screen'
+
+import style from '../Dock.module.css'
+
+const DockContextMenu = () => {
+    function handleClick(e: JSX.TargetedMouseEvent<HTMLDivElement>){
+        e.stopPropagation()
+    }
+    return (
+        <div
+            onClick={handleClick}
+            style={{position: 'absolute', top: '-50px'}}
+        >
+            hi
+        </div>
+    )
+}
+
+function updateWindowStatus(window: AppWindowConfig) {
+    let action;
+
+    if (window.isMinimized) {
+        action = 'ShowWindow' // UnMinimize the window and focus
+    }
+    else if (window.index === presentFocusedWindow.value.windowId) {
+        action = 'MinimizeWindow' //Minimize the current focsed window
+    }
+    else {
+        action = 'FocusWindow' // Focus the window
+    }
+
+    //If app is minimized
+    switch (action) {
+
+        case 'ShowWindow':
+            activeWindows.value = activeWindows.value.map((window_temp) => {
+                if (window_temp.index !== window.index) {
+                    return window_temp
+                }
+                else {
+                    return {
+                        ...window_temp,
+                        isMinimized: false
+                    }
+                }
+            })
+
+            presentFocusedWindow.value = {
+                isActive: true,
+                windowId: window.index
+            }
+            break
+
+        case 'MinimizeWindow':
+            activeWindows.value = activeWindows.value.map((window_temp) => {
+                if (window_temp.index !== window.index) {
+                    return window_temp
+                }
+                else {
+                    return {
+                        ...window_temp,
+                        isMinimized: true
+                    }
+                }
+            })
+
+            presentFocusedWindow.value = {
+                windowId: 0,
+                isActive: false
+            }
+            break
+
+        case 'FocusWindow':
+            presentFocusedWindow.value = {
+                isActive: true,
+                windowId: window.index
+            }
+            break
+
+    }
+}
+
+
+const DockApp: FC<{ window: AppWindowConfig }> = ({ window }) => {
+    const [isRightClicked, setIsRightClicked] = useState<boolean>(false);
+
+    function showContextMenu(e: JSX.TargetedMouseEvent<HTMLDivElement>) {
+        e.preventDefault()
+        setIsRightClicked(true);
+    }
+
+    function handleClick(){
+        setIsRightClicked(false)
+        updateWindowStatus(window)
+    }
+
+    function handleBlur() {
+        setIsRightClicked(false)
+    }
+
+    function toggleAnimation(event: any) {
+        const target = event.currentTarget;
+        target.classList.add(style['clicked']);
+
+        setTimeout(() => {
+            target.classList.remove(style['clicked']);
+        }, 1000);
+    }
+
+    return (
+        <div
+            class={style['dock-item']}
+            tabIndex={0}
+            onContextMenu={showContextMenu}
+            onBlur={handleBlur}
+            onClick={(e) => {
+                toggleAnimation(e)
+                handleClick()
+            }}
+        >
+            <span class={style['app-icon']}>{window.title}</span>
+            {isRightClicked && <DockContextMenu />}
+            <div
+                style={!window.isMinimized ? { backgroundColor: 'var(--color-green)' } : {}}
+                class={style['active-indicator']}
+            >
+            </div>
+        </div>
+    )
+}
+
+export default DockApp
