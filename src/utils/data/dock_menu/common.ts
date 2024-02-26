9e14118b427027571acd16c5ca81f94791f87926
chinmaypant21 <chinmay@growagro.in>
2024-02-26 15:35:30 +0530

    Dock menu

--- src/utils/data/dock_menu/common.ts
+++ src/utils/data/dock_menu/common.ts
@@ -3,7 +3,12 @@ export const optionsMenu: ContextMenu = [
         groupKey: 'g1',
         items: [
             { text: 'Remove from Dock', onclick: () => console.log('s'), disabled: true},
-            { text: 'Open at Login', onclick: () => console.log('s'), disabled: true},
+            { text: 'Open at Login', onclick: () => console.log('s')},
+        ]
+    },
+    {
+        groupKey: 'g2',
+        items: [
             { text: 'Show in Finder', onclick: () => console.log('s'), disabled: true},
         ]
     }
